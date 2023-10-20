eff5ea42612e6191f0a2934f8d0053cc5edfef8a
s.vahmi <s.vahmi@gmail.com>
2023-10-20 10:17:06 +0330

    Application layer implemented.

--- Code/Framework/Core/Framework.Core.Domian/Framework.Core.Domian/Data/IRepository.cs
+++ Code/Framework/Core/Framework.Core.Domian/Framework.Core.Domian/Data/IRepository.cs
@@ -1,19 +1,16 @@
 ﻿using Framework.Core.Domian.Aggregates;
 using System.Linq.Expressions;
-using Framework.Core.Domian.Entities;
 
-namespace Framework.Core.Domian.Data
+namespace Framework.Core.Domian.Data;
+
+public interface IRepository
 {
-    public interface IRepository
-    {
-    }
-    public interface IRepository<TId, TAggregateRoot> : IRepository where TAggregateRoot : AggregateRoot<TId> where TId : notnull
-    {
-        Task<TId> GetNextIdAsync(CancellationToken cancellationToken=default);
-        void Create(TAggregateRoot aggregate);
-        Task<TAggregateRoot?> LoadAsync(TId key, CancellationToken cancellationToken = default);
-        Task<bool> ExistAsync(TId key, CancellationToken cancellationToken = default);
-        Task<bool> ExistAsync(Expression<Func<TAggregateRoot, bool>> predicate,CancellationToken cancellationToken=default);
-    }
-  
+}
+public interface IRepository<TId, TAggregateRoot> : IRepository where TAggregateRoot : AggregateRoot<TId> where TId : notnull
+{
+    Task<TId> GetNextIdAsync(CancellationToken cancellationToken = default);
+
+    Task<TAggregateRoot?> LoadAsync(TId key, CancellationToken cancellationToken = default);
+
+    Task<bool> ExistAsync(Expression<Func<TAggregateRoot, bool>> predicate, CancellationToken cancellationToken = default);
 }
