d65cc01f81f36c9daa155497bdf68e92a1fd9dae
SajadVahmi <sajad.vahmi@gmail.com>
2024-05-22 14:13:13 +0330

    feature: player registration command and command handler implemented.

---
+++ Code/Games/Src/Games.Application/PlayerAggregate/CommandHandlers/PlayerCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using Framework.Core.Application.Commands;
+using Framework.Core.Contracts;
+using Games.Application.Contracts.PlayerAggregate.Commands;
+using Games.Application.PlayerAggregate.Factories;
+using Games.Domain.PlayerAggregate.Data;
+using Games.Domain.PlayerAggregate.Models;
+using Games.Domain.PlayerAggregate.Services;
+
+namespace Games.Application.PlayerAggregate.CommandHandlers;
+
+public class PlayerCommandHandler:ICommandHandler<RegisterPlayerCommand>
+{
+    private readonly IPlayerRepository _playerRepository;
+    private readonly IIdGenerator _idGenerator;
+    private readonly IClock _clock;
+    private readonly IDuplicateRegistrationCheckService _duplicateRegistrationCheckService;
+    private readonly IAuthenticatedUser _authenticatedUser;
+
+    public PlayerCommandHandler(IPlayerRepository playerRepository,IIdGenerator idGenerator,IClock clock,IDuplicateRegistrationCheckService duplicateRegistrationCheckService,IAuthenticatedUser authenticatedUser)
+    {
+        _playerRepository = playerRepository;
+        _idGenerator = idGenerator;
+        _clock = clock;
+        _duplicateRegistrationCheckService = duplicateRegistrationCheckService;
+        _authenticatedUser = authenticatedUser;
+    }
+    public async Task HandleAsync(RegisterPlayerCommand command, CancellationToken cancellationToken = default)
+    {
+        var registrationArgs = PlayerArgsFactory.CreateRegistrationArgs(
+            command: command,
+            idGenerator: _idGenerator,
+            clock: _clock,
+            duplicateRegistrationCheckService: _duplicateRegistrationCheckService,
+            authenticatedUser: _authenticatedUser);
+
+        var player = await Player.RegisterAsync(registrationArgs, cancellationToken);
+
+        await _playerRepository.AddAsync(player, cancellationToken);
+    }
+}
---
+++ Code/Games/Src/Games.Application/PlayerAggregate/Factories/PlayerArgsFactory.cs
@@ -0,0 +1,23 @@
+﻿using Framework.Core.Contracts;
+using Games.Application.Contracts.PlayerAggregate.Commands;
+using Games.Domain.PlayerAggregate.Arguments;
+using Games.Domain.PlayerAggregate.Services;
+
+namespace Games.Application.PlayerAggregate.Factories;
+
+public static class PlayerArgsFactory
+{
+    public static PlayerRegistrationArgs CreateRegistrationArgs(RegisterPlayerCommand command, IIdGenerator idGenerator,
+        IAuthenticatedUser authenticatedUser,IClock clock, IDuplicateRegistrationCheckService duplicateRegistrationCheckService)
+    {
+        return new PlayerRegistrationArgs()
+        {
+            Name = command.Name,
+            Family = command.Family,
+            UserId = authenticatedUser.GetSub()!,
+            Clock = clock,
+            IdGenerator = idGenerator,
+            DuplicateRegistrationCheckService = duplicateRegistrationCheckService
+        };
+    }
+}
